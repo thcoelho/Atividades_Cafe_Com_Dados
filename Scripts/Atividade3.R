3ca17f7cbd51cfd6c41d3a4081837864a897700e
Thiago Coelho <54235719+thcoelho@users.noreply.github.com>
2020-08-11 09:46:11 -0300

    Regressão Linear

--- Scripts/Atividade3.R
+++ Scripts/Atividade3.R
@@ -7,27 +7,33 @@ library(ggrepel)
 df <- read.csv("../Dados/Atividade3.csv", sep = ";")
 
 # Resolver compatibilidade trocando "," com ;
-df$Domicilios <-
-    as.numeric(sub(",", ".", df$Domicilios, fixed = TRUE))
+df$Domicílios <-
+    as.numeric(sub(",", ".", df$Domicílios, fixed = TRUE))
 df$GINI <- as.numeric(sub(",", ".", df$GINI, fixed = TRUE))
 
-
 # Beta Regression
-reg <- betareg(Domicilios ~ GINI, data = df)
+reg <- betareg(Domicílios ~ GINI, data = df)
+
+# Reg linear
+reglm <- lm(Domicílios ~  GINI, data=df)
+
+# Comparar resultados
+sum(residuals(reg))^2 < sum(residuals(reglm))^2
 
 # Sumário
-
-summary(reg)
+summary(reglm)
 
 # Gráfico
-ggplot(df, aes(GINI, Domicilios, label = Estado, color = Regiao)) +
+ggplot(df, aes(GINI, Domicílios, label = Estado, color = Região)) +
     geom_point(size = 2.5) +
-        geom_text_repel(color = "black") +
-        geom_line(aes(GINI, predict(reg)), color = "black") +
+        geom_text(data=df[19,], color = "black",
+                  nudge_y = -0.02) +
+        geom_line(aes(GINI, predict(reglm)), color = "black") +
     labs(title = "Desigualdade nos estados e porcentagem de 
 domicílios com auxílio emergencial",
      x = "Coeficiente de GINI",
-     y = "Domicílios com auxílio emergencial (%)")
+     y = "Domicílios com auxílio emergencial (%)") +
+    theme(plot.title = element_text(hjust = 0.5))
 
-# Salvar gráfico
-ggsave("../Graficos/Atividade3.jpg")        
+'# Salvar gráfico
+ggsave("../Graficos/Atividade3.png")        
